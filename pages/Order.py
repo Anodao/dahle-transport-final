ff2970523fcc39746638c781edc1c83af044e5ff
Anouar <90797364+Anodao@users.noreply.github.com>
2026-04-15 21:59:39 +0200

    Update Order.py

--- pages/Order.py
+++ pages/Order.py
@@ -36,10 +36,19 @@ st.markdown("""
     .step.active { background-color: #894b9d; color: white; border-color: #894b9d; }
     </style>
     
-<div class="nav-cta">
+<div class="navbar">
+        <div class="nav-logo">
+            <a href="/" target="_self"><img src="https://cloud-1de12d.becdn.net/media/original/964295c9ae8e693f8bb4d6b70862c2be/logo-website-top-png-1-.webp"></a>
+        </div>
+        <div class="nav-links">
+            <a href="/"><span>Hjem</span></a>
+            <span>Om oss</span><span>Tjenester</span><span>Galleri</span><span>Kontakt</span>
+        </div>
+        <div class="nav-cta">
             <a href="/Login" target="_self" class="cta-btn-outline">KUNDEPORTAL</a>
             <a href="/Order" target="_self" class="cta-btn">BESTILL TRANSPORT</a>
         </div>
+    </div>
 
 """, unsafe_allow_html=True)
 
